ae6e75124a7ce132c790a2cc96c7e3adf6535838
Mariola Rumlova <mariola.rumlova@enxoo.com>
2020-03-04 13:03:04 +0100

    Lookup component improved - more generic usage

--- force-app/main/default/lwc/lwcCatRegistration/lwcCatRegistration.js
+++ force-app/main/default/lwc/lwcCatRegistration/lwcCatRegistration.js
@@ -1,5 +1,7 @@
 import { LightningElement, api, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import apexSearch from '@salesforce/apex/CustomLookupController.search';
+//import insertVisit from '@salesforce/apex/REST_CheckIn.insertVisitProcess';
 
 export default class LwcCatRegistration extends LightningElement {
     @track name;
@@ -14,6 +16,17 @@ export default class LwcCatRegistration extends LightningElement {
     który dispatchuje event setTargetHotel z Id i Name hotelu. My to przechwytujemy
     a następnie ustawiamy to w naszym inpucie jako wartość
     */ 
+   @track isMultiEntry = false;
+   @track initialSelection = [
+    {
+        id: 'na',
+        sObjectType: 'na',
+        icon: 'standard:lightning_component',
+        title: 'Inital selection',
+        subtitle: 'Not a valid record'
+    }
+    ];
+    @track errors = [];
 
    constructor() {
         super();
@@ -53,5 +66,77 @@ export default class LwcCatRegistration extends LightningElement {
         );
     }
 
+    handleLookupTypeChange(event) {
+        this.initialSelection = [];
+        this.errors = [];
+        this.isMultiEntry = event.target.checked;
+    }
+
+    handleSearch(event) {
+        const tableNameString = event.target.name;
+        const request = {
+                            ...event.detail,
+                            tableName: tableNameString
+                        };
+        apexSearch(request)
+            .then(results => {
+                if (tableNameString==="Cat__c") {
+                    this.template
+                    .querySelectorAll('c-lookup')[0]
+                    .setSearchResults(results);
+                } else {
+                    this.template
+                    .querySelectorAll('c-lookup')[1]
+                    .setSearchResults(results);
+                }
+            })
+            .catch(error => {
+                this.notifyUser(
+                    'Lookup Error',
+                    'An error occured while searching with the lookup field: ',
+                    'error'
+                );
+                // eslint-disable-next-line no-console
+                console.error('Lookup error', JSON.stringify(error));
+                this.errors = [error];
+            });
+    }
+
+    handleSelectionChange() {
+        this.errors = [];
+    }
+
+    handleSubmit() {
+        this.checkForErrors();
+        if (this.errors.length === 0) {
+            this.notifyUser('Success', 'The form was submitted.', 'success');
+        }
+    }
+
+    checkForErrors() {
+        const selection = this.template
+            .querySelector('c-lookup')
+            .getSelection();
+        if (selection.length === 0) {
+            this.errors = [
+                { message: 'You must make a selection before submitting!' },
+                { message: 'Please make a selection and try again.' }
+            ];
+        } else {
+            this.errors = [];
+        }
+    }
+
+    notifyUser(title, message, variant) {
+        if (this.notifyViaAlerts) {
+            // Notify via alert
+            // eslint-disable-next-line no-alert
+            alert(`${title}\n${message}`);
+        } else {
+            // Notify via toast
+            const toastEvent = new ShowToastEvent({ title, message, variant });
+            this.dispatchEvent(toastEvent);
+        }
+    }
 
 }
